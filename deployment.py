2e53612524e9dab4a02f59ba62ca6d02dbfc6ccc
binnu6757 <reddysab11@gmail.com>
2024-07-08 23:07:28 +0530

    Update deployment.py

--- deployment.py
+++ deployment.py
@@ -15,10 +15,8 @@ bathrooms = st.number_input("Enter the number of bathrooms",min_value = 0,max_va
 Neighbourhood = st.radio("Enter the neighbourhood",["Rural","Urban","Suburb"])
 neighbour = 1 if Neighbourhood=="Rural" else 2 if Neighbourhood == "Urban" else 3
 Yearbuilt = st.number_input("Enter the year of construction",min_value = 1900,max_value = 2030,step =1)
-numeric_data = [["squarefeet","Bedrooms","bathrooms","neighbour","Yearbuilt"]]
+price = model.predict([[squarefeet,Bedrooms,bathrooms,neighbour,Yearbuilt]])
+st.write("The price of the plot for the given details is Rs.",price)
 
-numeric_data = np.array([[1500, 3, 2, 1, 2005]])
 
-price = model.predict(numeric_data)
 
-st.write("The Price for given flat details is Rs.",price)
